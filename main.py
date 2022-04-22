0107e7aaaffa0f9ae3705bfb9a3364f8d9cf8848
Zacky2613 <84255182+Zacky2613@users.noreply.github.com>
2022-04-22 20:24:11 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import time
 
 ''' Python-Kahoot-Bot Copyright (c) 2022 Zacky2613 MIT License
 
-    ███████╗ █████╗  ██████╗██╗  ██╗██╗   ██╗██████╗  ██████╗ ██╗██████╗ 
-    ╚══███╔╝██╔══██╗██╔════╝██║ ██╔╝╚██╗ ██╔╝╚════██╗██╔════╝███║╚════██╗
-      ███╔╝ ███████║██║     █████╔╝  ╚████╔╝  █████╔╝███████╗╚██║ █████╔╝
-     ███╔╝  ██╔══██║██║     ██╔═██╗   ╚██╔╝  ██╔═══╝ ██╔═══██╗██║ ╚═══██╗
-    ███████╗██║  ██║╚██████╗██║  ██╗   ██║   ███████╗╚██████╔╝██║██████╔╝
-    ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝╚═════╝ 
+    ███████╗ █████╗  ██████╗██╗  ██╗██╗   ██╗  ██████╗  ██████╗ ██╗██████╗ 
+    ╚══███╔╝██╔══██╗██╔════╝██║ ██╔╝╚██╗ ██╔╝  ╚════██╗██╔════╝███║╚════██╗
+      ███╔╝ ███████║██║     █████╔╝  ╚████╔╝    █████╔╝███████╗╚██║ █████╔╝
+     ███╔╝  ██╔══██║██║     ██╔═██╗   ╚██╔╝    ██╔═══╝ ██╔═══██╗██║ ╚═══██╗
+    ███████╗██║  ██║╚██████╗██║  ██╗   ██║     ███████╗╚██████╔╝██║██████╔╝
+    ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝     ╚══════╝ ╚═════╝ ╚═╝╚═════╝ 
 
-Guthub Repo: https://github.com/Zacky2613/Python-Kahoot-Bot '''
+Github Repo: https://github.com/Zacky2613/Python-Kahoot-Bot '''
 
 
 def Kahoot_JoinLoop(game_pin, bot_name_input, bot_amount, join_time):
 
     bot = client()
-    bot_name_number_string_var = -3
+    bot_name_number_string_var = 0
   
     print(f"Joining Server: {game_pin}")
     print("WARNING: bots will say 'connected' even if the game pin is invaild!\n")
@@ -33,7 +33,7 @@ def Kahoot_JoinLoop(game_pin, bot_name_input, bot_amount, join_time):
         bot_name_number_string_var += 1
         time.sleep(join_time)
 
-def Joinloop(bind):
+def Joinloop():
     try:
         game_pin = int(room_key_entry.get())
         bot_name_input = bot_name_input_entry.get()
@@ -58,22 +58,26 @@ if __name__ ==  "__main__":
     buffer = tk.Label(root, text=" ").pack(side = "top", anchor = "nw") # These buffers are used for seperating the input boxes to make it look cleaner.
 
     room_key_label = tk.Label(root, text="Room Key:").pack(side = "top", anchor = "nw")
-    room_key_entry = tk.Entry(root).pack(side = "top", anchor = "nw")
+    room_key_entry = tk.Entry(root)
+    room_key_entry.pack(side = "top", anchor = "nw")
 
     buffer2 = tk.Label(root, text=" ").pack(side = "top", anchor = "nw")
 
     bot_name_input_label = tk.Label(root, text="Bot Name:").pack(side = "top", anchor = "nw")
-    bot_name_input_entry = tk.Entry(root, width = 25).pack(side = "top", anchor = "nw")
+    bot_name_input_entry = tk.Entry(root, width = 25)
+    bot_name_input_entry.pack(side = "top", anchor = "nw")
 
     buffer3 = tk.Label(root, text=" ").pack(side = "top", anchor = "nw")
 
     bot_amount_label = tk.Label(root, text="Amount of Bots: ").pack(side = "top", anchor = "nw")
-    bot_amount_entry = tk.Entry(root, width = 10).pack(side = "top", anchor = "nw")
+    bot_amount_entry = tk.Entry(root, width = 10)
+    bot_amount_entry.pack(side = "top", anchor = "nw")
 
     buffer4 = tk.Label(root, text=" ").pack(side = "top", anchor = "nw")
 
     bot_offset_label = tk.Label(root, text="Bot Joining offset second: ").pack(side = "top", anchor = "nw")
-    bot_offset_entry = tk.Entry(root, width = 5).pack(side = "top", anchor = "nw")
+    bot_offset_entry = tk.Entry(root, width = 5)
+    bot_offset_entry.pack(side = "top", anchor = "nw")
 
     buffer5 = tk.Label(root, text=" ").pack(side = "top", anchor = "nw")
 
@@ -83,7 +87,6 @@ if __name__ ==  "__main__":
     def close(bind):
         root.destroy()
 
-    root.bind("<Return>", Joinloop)
     root.bind("<Escape>", close)
 
     root.mainloop()
